2a2daf33e58e702efbf61441de76eaea893138aa
aydar013 <121465617+aydar013@users.noreply.github.com>
2023-02-27 13:24:03 -0800

    project 4 first commit

--- scripts/index.js
+++ scripts/index.js
@@ -25,18 +25,74 @@ const initialCards = [
   },
 ];
 
+//// PROFILE
+
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+
+//// MODALS
+
 const modal = document.querySelector(".modal");
+const container = document.querySelector(".modal__container");
+const form = document.querySelector(".modal_form");
+
+//// INPUTS AND BUTTONS
+
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".modal__close-button");
+const inputName = document.querySelector("#profile-title-input");
+const inputDescription = document.querySelector("#profile-description-input");
 
-document
-  .querySelector(".profile__edit-button")
-  .addEventListener("click", function () {
-    modal.classList.toggle("modal_opened");
-  });
+//// WRAPPERS
+const placesList = document.querySelector(".gallery__cards");
 
-document
-  .querySelector(".modal__close-button")
-  .addEventListener("click", function () {
-    modal.classList.toggle("modal_opened");
-  });
+/////////////
+/// FUNCTIONS
+/////////////
+
+function openModal() {
+  modal.classList.toggle("modal_opened");
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+
+function closeModal() {
+  modal.classList.toggle("modal_opened");
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closeModal();
+}
+
+function getCardElement(data) {
+  const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".places__item");
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardTitle.textContent = data.name;
+  cardImage.alt = data.name;
+
+  return cardElement;
+}
+
+function renderCard(data, wrapper) {
+  wrapper.append(getCardElement(data));
+}
+
+/////////////
+/// EVENT HANDLERS
+/////////////
+
+editButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+form.addEventListener("submit", handleFormSubmit);
+
+initialCards.forEach((data) => renderCard(data, placesList));
